6bffacad2917d8acb7301adbb87dfe3eb02b5ec1
Maim <jhilum@hotmail.com>
2018-12-16 18:22:38 +0200

    Update FormInput.js

--- components/FormInput.js
+++ components/FormInput.js
@@ -2,30 +2,14 @@ import React, { Component } from 'react'
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 
 class FormInput extends Component {
-
-  state = {
-    input: ''
-  }
-
-  onChangeText = (text) => {
-    const { validationRegex } = this.props
-
-    if (!text.match(validationRegex)) return
-
-    this.setState({ input: text })
-  }
-
   render() {
-    console.log('FOrm Input render')
-    const { text, placeholder } = this.props
-    const { input } = this.state
+    const { placeholder, error, ...props } = this.props
 
     return (
         <TextInput
-          style={styles.input}
+          style={[styles.input, error && styles.error]}
           placeholder={placeholder}
-          value={input}
-          onChangeText={this.onChangeText}
+          {...props}
         />
     )
   }
@@ -38,6 +22,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     padding: 5,
     borderRadius: 10
+  },
+  error: {
+    borderColor: 'red'
   }
 })
 
